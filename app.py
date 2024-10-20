4fa1d2df0ef0552220f4b4e672d4b11527917ccb
Kavyansh Bagdi <kavyanshbagdi224@gmail.com>
2024-10-20 20:20:41 +0530

    upload page is added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request,render_template
-
-# data
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
 engineering_branches = [
     "Artificial Intelligence and Data Engineering",
     "Chemical Engineering",
@@ -11,31 +11,31 @@ engineering_branches = [
     "Mechanical Engineering",
     "Metallurgical and Materials Engineering"
 ]
-course_codes = [
-    "CST205", "CST206", "CST207", "CST208", "CST209", "CST210", "CST301", 
-]
-years = [
-    2024,
-    2023,
-    2022,
-    2021,
-    2020,
-    2019,
-    2018,
-    2017,
-    2016,
-    2015,
-    2014,
-]
+semester = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII']
+year = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'mysql://root:password123@localhost/'
+ 
+mysql = MySQL(app)
 
 app = Flask(__name__, template_folder='templates',static_folder='static')
 
 @app.route('/')
 def home():
-    return render_template('index.html',engineering_branches=engineering_branches,course_codes=course_codes,years=years)
-@app.route('/upload')
+    return render_template('index.html')
+@app.route('/upload',methods = ['get','post'])
 def upload():
-    return render_template('upload.html')
+    return render_template('upload.html',branch = engineering_branches,sem=semester,y=year)
 
 if __name__ == "__main__":
     app.run(debug = True,host = "0.0.0.0",port="5000");
+
+
+def addData():
+    pass
+
+def readData(something):
+    cursor = mysql.connection.cursor()
+    queue = 'SELECT DISTINCT ' + something + ' FROM questionpaper;'
+    pass
